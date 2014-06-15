d1beebf46a80b69d858a69d7330de81878be3693
Jacky <legenddcr@gmail.com>
2014-06-16 00:51:16 +0800

    Add function maxargs and interp into main file.

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 /* This file is intentionally empty.  You should fill it in with your
    solution to the programming exercise. */
+#include "prog1.h"
+
+int maxargs(A_stm stm)
+{
+  return 0;
+}
+
+
+void interp(A_stm stm)
+{
+   
+}
 
 int main(int argc, char *argv[])
 {
+  A_stm stm = prog();
+  printf ("Max args is %d\n", maxargs(stm));
+  interp(stm);  
   return 0;
 }
 
