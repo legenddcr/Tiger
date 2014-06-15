6af528809873b659dc09302593fbc4ab3092c69f
legenddcr <legenddcr@gmail.com>
2014-06-16 00:59:27 +0800

    Add comments for maxargs function

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 /* This file is intentionally empty.  You should fill it in with your
    solution to the programming exercise. */
+#include <stdio.h>
+
+#include "util.h"
+#include "slp.h"
 #include "prog1.h"
 
+/* Calculate print num */
 int maxargs(A_stm stm)
 {
   return 0;
