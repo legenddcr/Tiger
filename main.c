cb953e5025bd567b223a6d62dd933795ba48376a
Chengrui Deng <chengrui.deng@intel.com>
2014-06-23 18:22:10 +0800

    Implement print calculation

--- main.c
+++ main.c
@@ -6,10 +6,42 @@
 #include "slp.h"
 #include "prog1.h"
 
+int maxargsExp(A_exp exp)
+{
+  if (exp == NULL)
+    return 0;
+  
+  if (exp->kind == A_eseqExp)
+    return maxargs(exp->u.eseq.stm) + maxargsExp(exp->u.eseq.exp);
+  else if (exp->kind == A_opExp)
+    return maxargsExp(exp->u.op.left) + maxargsExp(exp->u.op.right);
+  else return 0;
+}
+
+int maxargsExpList(A_expList expList)
+{
+  if (expList == NULL)
+    return 0;
+
+  if (expList->kind == A_pairExpList)
+    return maxargsExp(expList->u.pair.head) + maxargsExpList(expList->u.pair.tail);
+  else
+    return maxargsExp(expList->u.last);
+}
+
 /* Calculate print num */
 int maxargs(A_stm stm)
 {
-  return 0;
+  /* traverse the tree and to calculate PrintStm node number */
+  if (stm == NULL)
+    return 0;
+
+  if (stm->kind == A_compoundStm)
+    return maxargs(stm->u.compound.stm1) + maxargs(stm->u.compound.stm2);
+  else if (stm->kind == A_assignStm)
+    return maxargsExp(stm->u.assign.exp);
+  else
+    return 1 + maxargsExpList(stm->u.print.exps);
 }
 
 
